2b2405b7b3ca3a51303eee07ac3c408e2cbef501
praveshgoyal20 <68349132+praveshgoyal20@users.noreply.github.com>
2020-07-16 00:20:16 +0530

    Add files via upload

---
+++ Basic.py
@@ -0,0 +1,16 @@
+# Assign value to a variable
+x=10
+
+# Assign value to multiple variables
+x,y=20,30
+
+# Assign same value to multiple variables
+x=y=100
+print('x value is', x)
+print('y value is', y)
+
+a=1
+b=2
+a,b = b,a
+print('a value is', a)
+print('b value is', b)
